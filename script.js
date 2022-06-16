ca35fca497139b08a6b612e2812548c00853c036
Elnura Kubanychbekova <elnura.kubanychbekova.alt@gmail.com>
2022-06-16 11:06:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+//1. Напишите программу, которая считает **количество секунд** в часе.
+
+/*let hour = prompt("Канча саат?");
+let minutsToHour = 60;
+let secondsToHour = 3600;
+let result = hour * minutsToHour;
+let result2 = hour * secondsToHour;
+document.write(result + " " + "минута бар");
+document.write(result2 + " " + "секундa бар");*/
+
+/*2. Создайте переменную `myNumber` и присвойте ей значение 2022. Выведите значение этой переменной на экран с помощью метода **alert.**
+
+let myNumber = 2022;
+alert(myNumber);*/
+
+/*3. Спросите имя пользователя с помощью метода **prompt**. Выведите сообщение на страницу с помощью **`document.write`** : **“Рад знакомству {имя пользователя}!”**.
+
+const user = prompt("Как Вас зовут?");
+document.write("Рад знакомству  " + user + "!");*/
+
+/*4.Даны два числа a, b. Написать программу, которая считает среднее арифметическое двух чисел.
+
+let a = parseInt(prompt(" a мааниси"));
+let b = parseInt(prompt(" b мааниси"));
+let result = (a + b) / 2;
+
+document.write(result);*/
+
+/*5. Спросите пользователя ввести любое число. Ваша задача вывести следующий по счету число. Даны два числа `a, b`. Написать программу, которая считает среднее . *Пример: Пользователь вводит число 10. Программа выводит число 11.
+
+let nextNumber = prompt("бир сан  бериниз");
+nextNumber++;
+document.write(`${nextNumber}`);*/
+
+/*6. Создайте объект описывающий пользователя, в данном объекте должны быть следующие ключи:
+    - Имя
+    - Возраст
+    - Список друзей
+    - Почтовый адрес
+    - Электронный адрес
+
+const user = {
+  name: "Elnura",
+  age: 22,
+  friends: ["Ayday", "Aynura"],
+  postalCode: "720000",
+  email: "elnura.kubanychbekova.alt@gmail.com",
+};
+console.log(user);*/
+
+/*7. Создайте объект описывающий блог-пост на сайте:
+    - Название поста
+    - Автор поста
+    - Дата публикации поста
+    - Тело поста (содержит текст поста)
+    - Картинка поста (содержит url картинки)
+    - Теги поста
+const cheberAyim = {
+  name: "Capsule Wardrobe",
+  author: "Aynura",
+  publishedAt: 2022,
+  postBody:
+    "A capsule wardrobe is a limited selection of interchangeable clothing pieces that complement each other. These are often classic pieces that do not go out of style and are primarily composed of neutral colors. A capsule wardrobe allows you to create a variety of different outfits with a small selection of clothes.",
+
+  postImg:
+    "https://images.squarespace-cdn.com/content/v1/547a3834e4b053a861c4874e/1616705868398-8WEIP19CGKGV3LEC17HS/how-to-build-a-capsule-wardrobe?format=1000w",
+  tags:[ "capsule wardrobe", "wardrobe", 'clothing']                                                  
+};
+console.log(cheberAyim);*/
+
+/*8. Создайте массив с именами пользователей.
+
+let brothers = ["Эдилбек", "Акылбек", "Асылбек"];
+console.log(brothers);*/
+
+/*9.Создайте массив с url адресами на разные картинки
+
+let pets = [
+  "https://i.cbc.ca/1.5077459.1553886010!/fileImage/httpImage/pets.jpg",
+  "https://lizasenglish.ru/wp-content/uploads/2017/06/animals.jpg",
+  "https://www.dsm.com/anh/species/pets.thumb.800.480.png",
+];*/
+
+/*10 - Создайте массив содержащий объекты описывающие блог-посты (схема объектов идентична как в пункте 7.
+let blogPost = [
+  { name: "Jyldyz Academy" },
+  { author: "Timur Mone." },
+  { publishedAt: 2017 },
+  { postImg: "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"},
+  { postBody: "Jyldyz Academy - это сообщество цифровых кочевников, наше коллективное видение - сделать обучение 21 века реальностью для всех."},
+  {  postsTag: '<img src="">' },
+];*/
